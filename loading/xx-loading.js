8cd5a9ab7eee36200e87e08072715dce4304aa95
linwens <lws200841001@gmail.com>
2018-08-30 23:39:51 +0800

    ...

--- loading/xx-loading.js
+++ loading/xx-loading.js
@@ -26,35 +26,46 @@
 			var $val = this.$val = document.getElementById(selector);
 			return $val;
 		},
-		progress:function(dist, cb){//进度主函数
-			console.log(this);
-			console.log(xLoading.fn);
+		progress:function(phase, speed, cb){//进度主函数(进度值，数字变化时间间隔，回调函数)
 			var _self = this;
-			this.timer = window.setInterval(function(){
-				if(_self.prg>=dist){
+			var _speed = this.random(speed);//随机化间隔时间，更真实
+			this.timer = window.setTimeout(function(){
+				console.log(_self.prg);
+				if(_self.prg>=phase){
 					window.clearInterval(_self.timer);
-					_self.prg=dist
+					_self.prg=phase
 					cb&&cb();
 				}else{
 					_self.prg++;
 					_self.$prg.style.width = _self.prg+'%';
 					_self.$val.innerText = _self.prg+'%';
+					_self.progress(phase, _speed, cb);
 				}
-			}, 100);
+			}, _speed);//间隔时间动态
 		},
-		add:function(dist, cb){//传值
+		add:function(phase, speed, cb){//传值
 			//传得值要和当前已经累加到的值next作比较
-			
-			this.progress(this.next,cb);
+			// if(this.next + phase >=100){
+			// 	this.next = 100;
+			// }else{
+			// 	this.next += phase;
+			// }
+			this.progress(phase, speed, cb);
 		},
-		complete:function(){},
-		random:function(){}
+		complete:function(speed,cb){
+			console.log('do complete');
+			this.progress(100, speed, cb);
+		},
+		random:function(n){
+
+			return n+(100-Math.random()*200);//{n-100,n+100}
+		}
 	}
 	//设置实例
 	var init = xLoading.fn.init = function(box){
 		this.prg = 0;//进度条具体值
 		this.timer = 0;//定时器
-		this.next = this.prg;//增量进度，实现
+		//this.next = this.prg;//增量进度，实现
 		this[0] = document.getElementById(box);//指定进度条根dom元素
 		return this;
 	}
