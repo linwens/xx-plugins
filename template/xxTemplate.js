3e64216c1d2ac93affd6361a2210c519b3aa0d5e
linwens <lws200841001@gmail.com>
2018-09-11 17:40:48 +0800

    nothing

--- template/xxTemplate.js
+++ template/xxTemplate.js
@@ -2,7 +2,8 @@
  *思路：
  *  1、underscore.js源码1.9.1：https://github.com/jashkenas/underscore/blob/master/underscore.js
  *  2、廖雪峰教程：https://www.liaoxuefeng.com/article/001426512790239f83bfb47b1134b63b09a57548d06e5c5000
- *
+ *  3、冴羽的文章：https://segmentfault.com/a/1190000012501120
+ *  4、
  *设计模式：
  *
  *调用方式：
@@ -16,17 +17,18 @@
  *  4、
  *
 **/
-;(function(window, document){
-	var xxTPL = function(){
+; (function (window, document) {
+	var xxTPL = function () {
 		//定义函数体内容
 		var code = '';
 		//声明函数
 		var fn = new Function(code);//这样声明的函数的最后一个参数是个字符串，它包含整个函数体的代码
 		//声明渲染函数，为了链式调用，所以使用this
-		this.render = function(model){
+		this.render = function (model) {
 			return fn.apply(model);
 		}
 	};
 
 	window.xxTPL = xxTPL;
+	
 })(window, document);
