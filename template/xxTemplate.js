e6eb8f844f2e0481e1f7f5238a8336f23d791b86
linwens <lws200841001@gmail.com>
2018-09-17 00:36:53 +0800

    go copy underscore

--- template/xxTemplate.js
+++ template/xxTemplate.js
@@ -5,7 +5,9 @@
  *  3、冴羽的文章：https://segmentfault.com/a/1190000012501120
  *  4、
  *设计模式：
- *
+ *涉及的关键方法：
+ *  1、new Function();
+ *  2、stringObject.replace(regexp/substr, replacement/fn);https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/replace
  *调用方式：
  *  1、xxTPL(tpl, html)
  *  2、支持require引入
@@ -20,12 +22,47 @@
 ; (function (window, document) {
 	var xxTPL = function (tpl) {
 		//定义函数体内容
-		var code = "var p = []; p.push('";
-		code += tpl.replace(/[\r\t\n]/g, "")
-			.replace(/<%=(.*?)%>/g, "');p.push($1);p.push('")
-			.replace(/<%/g, "');")
-			.replace(/%>/g, "p.push('");
-		code += "');return p.join('')"
+		// var code = "var p = []; p.push('";
+		// code += tpl.replace(/[\r\t\n]/g, "")
+		// 	.replace(/<%=(.*?)%>/g, "');p.push($1);p.push('")
+		// 	.replace(/<%/g, "');")
+		// 	.replace(/%>/g, "p.push('");
+		// code += "');return p.join('')";
+		
+		//处理特殊字符 单引号' 反斜杠\ 换行\n 回车\r 行分隔符u2028 段落分隔符u2029
+		var escapes = {//对应处理
+			"'": "'",
+			'\\':'\\',
+			'\r':'r',
+			'\n':'n',
+			'\u2028':'u2028',
+			'\u2029':'u2029'
+		};
+		//匹配特殊字符
+		var escapeReg = /\\|'|\r|\n|\u2028|\u2029/g;
+		//其实就是阻止反斜杠被转义
+		var escapeChar = function(m){
+			return '\\'+ escapes[m];
+		}
+		//设置插值、插入表达式对应的正则可配
+		var settings = {
+			eval:/<%=([\s\S]+?)%>/g, //插值
+			inter:/<%([\s\S]+?)%>/g, //插入表达式
+		}
+		//抄袭underscore拼接字符串
+		var code = "var __p = '';\n __p+='";
+		// code += tpl.replace(/[\r\t\n]/g, "")
+		// 	.replace(/<%=([\s\S]+?)%>/g, "'+$1+'")
+		// 	.replace(/<%([\s\S]+?)%>/g, "';$1 __p+='");
+		// code += "';return __p";
+		code += tpl.replace(escapeReg, escapeChar)
+			.replace(settings.eval, function(m,eval){
+				return "'+ "+ eval +"+'"
+			})
+			.replace(settings.inter, function(m,inter){
+				return "';\n "+ inter +"\n __p+='"
+			});
+		code += "';\n return __p";
 		//上面的正则匹配要优化
 
 
