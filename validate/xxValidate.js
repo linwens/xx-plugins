1698b90d50f7f17792425c7a3edafb53544f8bf0
linwens <lws200841001@gmail.com>
2018-12-12 20:22:09 +0800

    增加单选框/多选框校验，优化提示文案展示

--- validate/xxValidate.js
+++ validate/xxValidate.js
@@ -18,85 +18,66 @@
  * 调用方式：(dom, [{ strategy:'name[: other arg]', errMsg:'' }]),错误提示出现顺序依据调用顺序为准
  * 
  * 实现功能: 1. 不需要form表单,走ajax提交,每个input元素增加独立的校验;
- *          2. 可设置错误提示出现位置;(需要硬性规定dom结构?);
+ *          2. 可设置错误提示的样式;(需要硬性规定dom结构?);
  *          3. 最终返回一个Boolean值,确定是否校验通过(类似elementui的校验函数),参数是一个回调函数;
  *          4. 可自行扩展校验规则;
- *          5. 
+ *          5. checkbox/radio校验;
+ *          6. 设置校验的时间（blur,change,提交时统一校验）
+ *          7. 
  **/
 ;(function(window, document){
     //预先定义好(各种校验规则)策略，替换N个if判断
     var strategies = {
         // 是否为空
         isNoEmpty: function( val, errMsg){
-            if( val === '' ){
-                //if(cb){cb(this, errMsg)}
+            console.log(val)
+            if( val === '' || val.length==0){
                 return errMsg;
-            // }else{
-            //     removeTips(this)
             }
         },
         //最大长度
         maxLen:function( val, max, errMsg){
-            //console.log(arguments)
             if(val.length>max){
-                //if(cb){cb(this, errMsg)}
                 return errMsg;
-            // }else{
-            //     removeTips(this)
             }
         },
         //最小长度
         minLen:function( val, min, errMsg){
             if(val.length<min){
-                //if(cb){cb(this, errMsg)}
                 return errMsg;
-            //}else{
-            //    removeTips(this)
             }
         },
         //校验手机号
         isMobile: function( val, errMsg){
             if(!/(^1[3|5|8][0-9]{9}$)/.test( val )){
-                //if(cb){cb(this, errMsg)}
                 return errMsg;
-            //}else{
-            //    removeTips(this)
             }
         },
     };
     //一些工具函数
-    //给目标元素后面增加元素用于存放提示信息
-    var removeTips = function(t){
-        for(var i=0, cn; cn=t.parentNode.childNodes[i++];){
-            if(cn.attributes&&cn.attributes['prop']&&cn.attributes['prop'].nodeValue==='msgbox'){
-            //     //NodeList不是数组，所以不能用splice方法
-            //     if(m){
-            //         cn.innerText = m;
-            //     }else{
-                     t.parentNode.removeChild(cn);
-            //     }
-            //     return;
-            }
+    //自定义事件绑定函数
+    var on = function(tar, type, fn){
+        if(addEventListener){
+            tar.addeventListern(type,fn);
+        }else{
+            tar.attachEvent("on"+type, fn);
         }
     };
-    var createTips = function(t,m){
-        // console.log(t);
-        // console.log(m);
-        for(var i=0, cn; cn=t.parentNode.childNodes[i++];){
-            if(cn.attributes&&cn.attributes['prop']&&cn.attributes['prop'].nodeValue==='msgbox'){
-                //NodeList不是数组，所以不能用splice方法
-                if(m){
-                    cn.innerText = m;
-                }else{
-                    t.parentNode.removeChild(cn);
-                }
-                return;
-            }
+    //判断是否是checkbox
+    var isChcekbox = function(input){
+        if(input.length){//如果是一组多选框
+            return input[0].type === "checkbox"
+        }else{
+            return input.type === "checkbox";
         }
-        var msg = document.createElement('span');
-        msg.setAttribute('prop','msgbox');
-        msg.innerText = m;
-        t.parentNode.appendChild(msg);
+    };
+    //判断是否是radio
+    var isRadio = function(input){
+        if(input.length){//如果是一组多选框
+            return input[0].type === "radio"
+        }else{
+            return input.type === "radio";
+        }
     };
     //检查元素是否存在/重复。。。该方法要优化，比如filter的回调函数提取出来给调用者
     var isRepeat = function isRepeat(arr, name){
@@ -110,8 +91,32 @@
     var Validator = function(){
         this.cache = []; //保存校验规则
     };
+    var _proto = Validator.prototype;
+    //生成消息提示dom
+    _proto.insertHtml = function(tar, m){
+        //判断是否radio
+        if(tar.length&&tar.length>1){
+            tar = tar[tar.length-1]
+        }
+        
+        var tipsDom = tar.nextElementSibling;
+        if(tipsDom){
+            if(m){
+                tipsDom.innerText = m;
+            }else{
+                tar.parentNode.removeChild(tar.nextElementSibling); //如果错误提示为空，把dom删掉
+            }
+        }else{
+            if(m){ // 只有有错误信息的时候才生成提示dom
+                var msg = document.createElement('span');
+                msg.innerText = m;
+                tar.parentNode.insertBefore(msg, tar.nextElementSibling) //找到目标元素的下一个元素（如果不存在就加在目标元素的后面），然后替换成消息提示dom;
+            }
+        }
+        
+    };
     //保存校验规则的函数
-    Validator.prototype.add = function(dom, rules){
+    _proto.add = function(dom, rules){
         var _self = this;
         for(var i=0, rule; rule=rules[i++];){
             (function(rule){//通过闭包预存rule, 不然push的时候会有问题,而且访问不到rule变量
@@ -120,12 +125,22 @@
                 
                 _self.cache.push(function(){
                     var strategy = ary.shift(); // shift()把数组的第一个元素从其中删除，并返回第一个元素的值.
-                    ary.unshift(dom.value); //数组首部存入value
+                    if(isChcekbox(dom)){//如果是多选框，val是一个存储多个多选框的数组
+                        var val = [];
+                        dom.name = dom[0].name;//给dom增加name字段，方便后面start函数里走进判断，生成错误提示
+                        for(var i=0,item;item=dom[i++];i<dom.length){
+                            if(item.checked){
+                                val.push(item.value);
+                            }
+                        }
+                        ary.unshift(val)
+                    }else if(isRadio(dom)){
+                        dom.name = dom[0].name;
+                        ary.unshift(dom.value);
+                    }else{
+                        ary.unshift(dom.value); //数组首部存入value
+                    }
                     ary.push(errMsg); // 数组尾部存入errMsg
-                    // var cb = function(t,m){ //校验时执行的回调
-                    //     createTips(t,m)
-                    // }
-                    // ary.push(cb);
                     msg = strategies[strategy].apply(dom, ary); // 绑定this,返回函数
                     return {"dom":dom,"msg":msg};
                 })
@@ -133,46 +148,30 @@
         }
     };
     //启动校验的函数
-    Validator.prototype.start = function(){
+    _proto.start = function(){
         var valid = [];//用于存储返回结果
         var domCache = [];
         for(var i=0, validateFunc; validateFunc = this.cache[i++];){ // 遍历缓存的校验规则, 执行校验获取返回信息
             var inst = validateFunc();
-            console.log(inst)
             if(inst){//存在错误信息
                 if(inst.msg){
                     valid.push(inst);
                 }else{
                     if(isRepeat(valid,inst.dom.name)<1){//当前dom已存在，提示信息不删除
-                        removeTips(inst.dom)
+                        //removeTips(inst.dom)
+                        this.insertHtml(inst.dom,inst.msg)
                     }
                 }
-                //for(var j=0,item;item=valid[j++];){
                 var k = valid.indexOf(inst);
                 if(k>-1){//长度大于1
-                    if(isRepeat(valid,inst.dom.name)>1){
+                    if((isRepeat(valid,inst.dom.name)>1)){
                         continue;
                     }else{
-                        createTips(inst.dom,inst.msg)
+                        this.insertHtml(inst.dom,inst.msg)
+                        //createTips(inst.dom,inst.msg)
                     }
                 }
-                
-                
-                // if(k>-1&&valid[k].dom.name===inst.dom.name){
-                //     console.log('这个dom有了');
-                //     console.log(domCache[k])
-                //     continue;
-                // }else{
-                //     //domCache.push(inst.dom);
-                //     createTips(inst.dom,inst.msg)
-                // }
-                //}
             }
-            // if(valid.length==0){ // 如果存在返回信息说明校验没通过
-            //     return true;
-            // }else{
-            //     return false;
-            // }
         }
         console.log('-----valid----');
         console.log(valid)
