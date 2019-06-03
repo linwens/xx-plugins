56964f029e8fc8895813036d818a6412edb5bc9c
linwens <lws200841001@gmail.com>
2019-06-04 00:19:58 +0800

    抄入callbacks模块

--- Xepto/Xepto.js
+++ Xepto/Xepto.js
@@ -1871,5 +1871,116 @@
         }
 
     })(Xepto);
+    // 增加callbacks模块（deferred依赖的模块）
+    ;(function($) {
+        $.Callbacks = function(options) {
+            options = $.extend({}, options);
+            var memory,
+                fired,
+                firing,
+                firingStart,
+                firingLength,
+                firingIndex,
+                list = [],
+                stack = !options.once && [];
+            var fire = function(data) {
+                    memory = options.memory && data
+                    fired = true
+                    firingIndex = firingStart || 0
+                    firingStart = 0
+                    firingLength = list.length
+                    firing = true
+                    for ( ;list && firingIndex < firingLength; ++firingIndex) {
+                        if (list[firingIndex].apply(data[0], data[1]) === false && options.stopOnFalse) {
+                            memory = false
+                            break
+                        }
+                    }
+                    firing = false
+                    if (list) {
+                        if (stack) {
+                            stack.length && fire(stack.shift())
+                        } else if (memory) {
+                            list.length = 0
+                        } else {
+                            Callbacks.disable()
+                        }
+                    }
+                };
+            var Callbacks = {
+                add: function() {
+                    if (list) {
+                        var start = list.length,
+                            add = function(_, arg){
+                                $.each(args, function(_, arg) {
+                                    if (typeof arg === "function") {
+                                        if (!options.unique || !Callbacks.has(arg)) {
+                                            list.push(arg)
+                                        }
+                                    } else if (arg && arg.length && typeof arg !== 'string') {
+                                        add(arg)
+                                    }
+                                })
+                            };
+                        add(arguments)
+                        if (firing) {
+                            firingLength = list.length
+                        } else if (memory) {
+                            firingStart = start
+                            fire(memory)
+                        }
+                    }
+                    return this
+                },
+                remove: function() {
+                    if (list) {
+                        $.each(arguments, function(_, arg) {
+                            var index
+                            while((index = $.inArray(arg, list, index)) > -1) {
+                                list.splice(index, 1)
+                                if (firing) {
+                                    if (index <= firingLength) --firingLength
+                                    if (index <= firingIndex) --firingIndex
+                                }
+                            }
+                        })
+                    }
+                    return this
+                },
+                has: function(fn) {
+                    return !!(list && (fn ? $.inArray(fn, list) > -1 : list.length))
+                },
+                empty: function() {
+                    firingLength = list.length = 0
+                    return this
+                },
+                disable: function() {
+                    list = stack = memory = undefined
+                },
+                locked: function() {
+                    return !stack
+                },
+                fireWith: function(context, args) {
+                    if (list && (!fired || stack)) {
+                        args = args || []
+                        args = [context, args.slice ? args.slice() : args]
+                        if (firing) {
+                            stack.push(args)
+                        } else {
+                            fire(args)
+                        }
+                    }
+                    return this
+                },
+                fire: function() {
+                    return Callbacks.fireWith(this, arguments)
+                },
+                fired: function() {
+                    return !!fired
+                }
+            };
+            return Callbacks
+        }
+    })(Xepto);
     return Xepto
 }))
